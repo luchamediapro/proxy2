e3e013f564d7f4e1a0008a84e3a2a9039e162173
luchamediapro <entretenimientogaran@gmail.com>
2025-02-05 18:16:37 -0500

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-
 const app = express();
 
 app.use('/proxy', createProxyMiddleware({
     target: 'https://tarjetarojaenvivo.lat',
     changeOrigin: true,
     pathRewrite: { '^/proxy': '' },
-    onProxyRes: function (proxyRes) {
+    onProxyRes: function (proxyRes, req, res) {
         delete proxyRes.headers['x-frame-options'];
         delete proxyRes.headers['content-security-policy'];
     }
