7416485d5c347c93863e90a20b90545a24dedab8
luchamediapro <entretenimientogaran@gmail.com>
2025-02-05 19:45:59 -0500

    index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
+const request = require('request');
 const port = process.env.PORT || 3000;
 
-// Middleware para manejar errores
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
+// Middleware para manejar la ruta '/proxy/*'
+app.get('/proxy/*', (req, res) => {
+  const targetUrl = 'https://tarjetarojaenvivo.lat/player/' + req.params[0];
+  console.log(`Redirecting to: ${targetUrl}`);  // Verifica la URL que estás redirigiendo
+  request(targetUrl).pipe(res);  // Redirige la solicitud al sitio del video
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
