a08e20f474ce3cec3d697b5478041bfa6acf806c
luchamediapro <entretenimientogaran@gmail.com>
2025-02-05 20:11:54 -0500

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const app = express();
 
-// Configura el proxy
+// Configura el proxy para redirigir el video desde el enlace proporcionado
 app.use('/proxy', createProxyMiddleware({
-    target: 'https://tarjetarojaenvivo.lat', // URL de destino para el contenido del video
+    target: 'https://la12hd.com/vivo/canal.php?stream=foxsports2mx', // URL de destino
     changeOrigin: true, // Cambia el origen de la solicitud
     pathRewrite: { '^/proxy': '' }, // Elimina '/proxy' de la URL antes de enviarla
     selfHandleResponse: false, // No proceses la respuesta manualmente
@@ -14,33 +15,5 @@ app.use('/proxy', createProxyMiddleware({
     }
 }));
 
-// Configuración de la política de seguridad de contenido
-app.use((req, res, next) => {
-    res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'none'; object-src 'none';");
-    next();
-});
-
-// Rutea a un HTML con iframe que utiliza el proxy
-app.get('/video', (req, res) => {
-    const iframeHtml = `
-        <html>
-            <head>
-                <title>Video Embed</title>
-            </head>
-            <body>
-                <iframe 
-                    src="/proxy/player/1/71"  <!-- Utiliza el proxy para cargar el video -->
-                    width="800" 
-                    height="600" 
-                    sandbox="allow-scripts allow-same-origin"  <!-- Aquí se añade el atributo sandbox -->
-                    style="border:none;">
-                </iframe>
-            </body>
-        </html>
-    `;
-    res.send(iframeHtml);  // Responde con la página HTML que incluye el iframe
-});
-
-// Inicia el servidor en el puerto especificado
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Proxy corriendo en http://localhost:${port}`));
