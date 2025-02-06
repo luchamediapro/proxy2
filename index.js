58c70ff702e3055db7f0b520822764b9a87983ec
luchamediapro <entretenimientogaran@gmail.com>
2025-02-05 21:52:03 -0500

    index.js

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-
+const cors = require('cors');
 const app = express();
 
-// Configura el proxy para la ruta /proxy
+// Permitir CORS
+app.use(cors());
+
+// Configurar proxy
 app.use('/proxy', createProxyMiddleware({
-    target: 'https://www.telextrema.com', // URL de destino donde se aloja el video
+    target: 'https://www.telextrema.com',
     changeOrigin: true,
-    pathRewrite: { '^/proxy': '' }, // Elimina '/proxy' de la URL antes de enviarla
-    selfHandleResponse: false, // Deja que el proxy maneje la respuesta por sí mismo
-    onProxyReq: (proxyReq, req, res) => {
-        console.log(`Proxying request to: ${proxyReq.url}`); // Imprime la URL que está siendo redirigida
+    pathRewrite: {
+        '^/proxy': '', // Elimina la parte "/proxy" de la URL
     },
     onError: (err, req, res) => {
-        console.error('Error en el proxy:', err); // Si ocurre un error, se imprime en la consola
-        res.status(500).send('Error en el proxy'); // Responde con error 500
+        console.error('Error en el proxy:', err);
+        res.status(500).send('Error en el proxy');
     }
 }));
 
-// Establece el puerto para el servidor
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Servidor proxy corriendo en http://localhost:${port}`);
-});
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
