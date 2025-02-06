a9adc8577bf008a10af256697b565f009cebd54c
luchamediapro <entretenimientogaran@gmail.com>
2025-02-05 19:50:37 -0500

    index.js

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
-const request = require('request');
 const port = process.env.PORT || 3000;
 
 // Middleware para manejar la ruta '/proxy/*'
-app.get('/proxy/*', (req, res) => {
+app.get('/proxy/*', async (req, res) => {
   const targetUrl = 'https://tarjetarojaenvivo.lat/player/' + req.params[0];
-  console.log(`Redirecting to: ${targetUrl}`);  // Verifica la URL que estás redirigiendo
-  request(targetUrl).pipe(res);  // Redirige la solicitud al sitio del video
+  console.log(`Redirigiendo a: ${targetUrl}`);  // Verifica la URL que estás redirigiendo
+
+  try {
+    const response = await axios.get(targetUrl, {
+      responseType: 'stream'  // Especifica que esperamos un stream (para contenido multimedia)
+    });
+
+    // Establecer encabezados de respuesta (ajusta el tipo según sea necesario)
+    res.setHeader('Content-Type', 'video/mp4');  // Esto puede variar dependiendo del tipo de contenido
+
+    // Pipe de la respuesta al cliente
+    response.data.pipe(res); 
+  } catch (error) {
+    console.error('Error al obtener el contenido:', error);
+    res.status(500).send('Error al obtener el contenido.');
+  }
 });
 
+// Iniciar el servidor
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`El servidor está en ejecución en el puerto ${port}`);
 });
